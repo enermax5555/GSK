3318a22a39ad7c3bb138bab8ef84db7d3ed67ec6
RedFlash <stoqn.sisimen@gmail.com>
2025-06-25 22:52:50 +0300

    Gh pages showcase prototype

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,5 +12,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true
-  }
+  },
+  // Adjust this to match your GitHub repository name for GitHub Pages
+  base: '/GSK/' // Replace with your repo name
 })
